08c1226d121dbfee2f564ecb7104ad3ee38ec117
Andrew Klotz <agc.klotz@gmail.com>
2018-07-10 20:30:15 -0400

    bellman-ford for finding sequence

--- calc/paths.go
+++ calc/paths.go
@@ -1,7 +1,112 @@
 package calc
 
-import ()
+import (
+	"math"
+)
 
-func Get(matrix [][]float64) int {
-	return 3
+const inf = 2147483647
+
+type sequence struct {
+	Path        []int
+	ReturnValue float64
+}
+
+func GetSequences(matrix [][]float64) []sequence {
+	var sequences []sequence
+	paths := getPaths(matrix)
+
+	for _, path := range paths {
+		s := sequence{path, returnValue(matrix, path)}
+		sequences = append(sequences, s)
+	}
+
+	return sequences
+}
+
+func returnValue(matrix [][]float64, path []int) float64 {
+	value := float64(1)
+
+	for e := 0; e < len(path)-1; e++ {
+		i := path[e]
+		j := path[e+1]
+		value = value * matrix[i][j]
+	}
+
+	return value
+}
+
+func getPaths(m [][]float64) [][]int {
+	matrix := toLog(m)
+	length := len(matrix)
+	pre := make([]int, length)
+	dist := make([]float64, length)
+
+	for i := 0; i < length; i++ {
+		dist[i] = inf
+		pre[i] = -1
+	}
+
+	dist[0] = 0
+
+	// relax nodes
+	for k := 0; k < length; k++ {
+		for i := 0; i < length; i++ {
+			for j := 0; j < length; j++ {
+				if (dist[i] + matrix[i][j]) < dist[j] {
+					dist[j] = dist[i] + matrix[i][j]
+					pre[j] = i
+				}
+			}
+		}
+	}
+
+	// check for cycles
+	cyclic := make([]bool, length)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+			if (dist[i] + matrix[i][j]) < dist[j] {
+				dist[j] = dist[i] + matrix[i][j]
+				cyclic[j] = true
+			}
+		}
+	}
+
+	// calc sequences
+	var paths [][]int
+	for i := 0; i < len(cyclic); i++ {
+		if cyclic[i] != true {
+			continue
+		}
+		visited := make([]bool, length)
+		var seq []int
+		p := i
+
+		for p != -1 && visited[p] != true {
+			seq = append(seq, p)
+			visited[p] = true
+			p = pre[p]
+		}
+		seq = append([]int{i}, reverse(seq)...)
+		paths = append(paths, seq)
+	}
+	return paths
+}
+
+func toLog(matrix [][]float64) [][]float64 {
+	m := make([][]float64, len(matrix))
+	for i := 0; i < len(m); i++ {
+		m[i] = make([]float64, len(matrix[i]))
+		copy(m[i], matrix[i])
+		for j := 0; j < len(m); j++ {
+			m[i][j] = -math.Log(m[i][j])
+		}
+	}
+	return m
+}
+
+func reverse(n []int) []int {
+	for i, j := 0, len(n)-1; i < j; i, j = i+1, j-1 {
+		n[i], n[j] = n[j], n[i]
+	}
+	return n
 }
--- calc/paths_test.go
+++ calc/paths_test.go
@@ -1,13 +1,23 @@
 package calc
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetPaths(t *testing.T) {
-	matrix := [][]float64{{1, 2, 4}, {0.5, 1, 3}, {0.25, 0.5, 1}}
-	paths := Get(matrix)
-	expected := 41
-	assert.Equal(t, expected, paths, "they should be equaal!")
+	matrix := [][]float64{
+		[]float64{1, 2, 4},
+		[]float64{0.5, 1, 3},
+		[]float64{0.25, 0.5, 1},
+	}
+	expected := []sequence{
+		sequence{[]int{0, 1, 2, 0}, float64(1.5)},
+		sequence{[]int{1, 2, 1}, float64(1.5)},
+		sequence{[]int{2, 1, 2}, float64(1.5)},
+	}
+
+	paths := GetSequences(matrix)
+	assert.Equal(t, expected, paths, "sequences incorrect")
 }
