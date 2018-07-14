544a67db74c8137aa2cc2718adc58a25ff9465a9
Andrew Klotz <agc.klotz@gmail.com>
2018-07-14 18:05:25 -0400

    cli for running

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"tukohama/runner"
 	"tukohama/tradeapi"
 )
 
 func main() {
+	cmd := flag.String("cmd", "", "command to run")
+	flag.Parse()
+
+	switch *cmd {
+	case "full":
+		fullRun()
+	default:
+		fmt.Println("command required!")
+		os.Exit(1)
+	}
+}
+
+func fullRun() {
 	runner := runner.New(
 		tradeapi.ConcreteClient{},
 		tradeapi.StaticCurrencyMap,
